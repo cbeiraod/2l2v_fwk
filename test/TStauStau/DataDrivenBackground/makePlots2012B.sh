bfe79a99c6fdb7f2188ca8c8c2c0f3a7d9a3beea
Cristovao da Cruz e Silva <cbeiraod@cern.ch>
2015-06-09 18:36:28 +0100

    Added jsons and plotter scripts to plot only wjets and only selected data taking periods (Might need to be updated because of change to prompts)

---
+++ test/TStauStau/DataDrivenBackground/makePlots2012B.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+OUTDIR=/lustre/ncg.ingrid.pt/cmslocal/cbeiraod/DDBkgFRPlots
+INDIR=/lustre/ncg.ingrid.pt/cmslocal/cbeiraod/DDBkgStudyFR
+runPlotterFWLite --noPowers --iEcm 8 --iLumi 19672 --inDir $INDIR/ --outDir $OUTDIR/ --outFile $OUTDIR/plotter.root --json tstaustau_samples_2012B.json --plotExt .png --plotExt .root
+
+OUTDIR=/lustre/ncg.ingrid.pt/cmslocal/cbeiraod/DDBkgPRPlots
+INDIR=/lustre/ncg.ingrid.pt/cmslocal/cbeiraod/DDBkgStudyPR
+#runPlotterFWLite --noPowers --iEcm 8 --iLumi 19672 --inDir $INDIR/ --outDir $OUTDIR/ --outFile $OUTDIR/plotter.root --json tstaustau_samples_2012B.json --plotExt .png --plotExt .root
+
+OUTDIR=/lustre/ncg.ingrid.pt/cmslocal/cbeiraod/DDBkgPRPlotsDYOnly
+INDIR=/lustre/ncg.ingrid.pt/cmslocal/cbeiraod/DDBkgStudyPR
+#runPlotterFWLite --noPowers --iEcm 8 --iLumi 19672 --inDir $INDIR/ --outDir $OUTDIR/ --outFile $OUTDIR/plotter.root --json tstaustau_samples_prompt.json --plotExt .png --plotExt .root
