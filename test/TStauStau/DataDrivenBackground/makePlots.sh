b1df7dc07a7faabffe55943152cf4886fd61ab55
Cristovao da Cruz e Silva <cbeiraod@cern.ch>
2015-06-09 18:35:03 +0100

    Modified where the plotter gets the files to account for the modification to the prompts

--- test/TStauStau/DataDrivenBackground/makePlots.sh
+++ test/TStauStau/DataDrivenBackground/makePlots.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 
+OUTDIR=/lustre/ncg.ingrid.pt/cmslocal/cbeiraod/DDBkgPlots
+INDIR=/lustre/ncg.ingrid.pt/cmslocal/cbeiraod/DDBkgStudy
+runPlotterFWLite --noPowers --iEcm 8 --iLumi 19672 --inDir $INDIR/ --outDir $OUTDIR/ --outFile $OUTDIR/plotter.root --json tstaustau_samples_full.json --plotExt .png --plotExt .root
+
 OUTDIR=/lustre/ncg.ingrid.pt/cmslocal/cbeiraod/DDBkgFRPlots
 INDIR=/lustre/ncg.ingrid.pt/cmslocal/cbeiraod/DDBkgStudyFR
-runPlotterFWLite --noPowers --iEcm 8 --iLumi 19672 --inDir $INDIR/ --outDir $OUTDIR/ --outFile $OUTDIR/plotter.root --json tstaustau_samples_full.json --plotExt .png --plotExt .root
+#runPlotterFWLite --noPowers --iEcm 8 --iLumi 19672 --inDir $INDIR/ --outDir $OUTDIR/ --outFile $OUTDIR/plotter.root --json tstaustau_samples_full.json --plotExt .png --plotExt .root
 
 OUTDIR=/lustre/ncg.ingrid.pt/cmslocal/cbeiraod/DDBkgPRPlots
 INDIR=/lustre/ncg.ingrid.pt/cmslocal/cbeiraod/DDBkgStudyPR
-runPlotterFWLite --noPowers --iEcm 8 --iLumi 19672 --inDir $INDIR/ --outDir $OUTDIR/ --outFile $OUTDIR/plotter.root --json tstaustau_samples_full.json --plotExt .png --plotExt .root
+#runPlotterFWLite --noPowers --iEcm 8 --iLumi 19672 --inDir $INDIR/ --outDir $OUTDIR/ --outFile $OUTDIR/plotter.root --json tstaustau_samples_full.json --plotExt .png --plotExt .root
 
 OUTDIR=/lustre/ncg.ingrid.pt/cmslocal/cbeiraod/DDBkgPRPlotsDYOnly
 INDIR=/lustre/ncg.ingrid.pt/cmslocal/cbeiraod/DDBkgStudyPR
