4c2e9b59fcc1875b9252fa231c411f446b1ba344
Cristovao da Cruz e Silva <cbeiraod@cern.ch>
2015-06-09 18:34:34 +0100

    Part of previous commit

--- test/TStauStau/DataDrivenBackground/runAnalysis.sh
+++ test/TStauStau/DataDrivenBackground/runAnalysis.sh
@@ -1,25 +1,13 @@
 #!/bin/bash
 
-#Fake Rate
-OUTDIR=/lustre/ncg.ingrid.pt/cmslocal/cbeiraod/DDBkgStudyFR
+OUTDIR=/lustre/ncg.ingrid.pt/cmslocal/cbeiraod/DDBkgStudy
 
 JSONFILE=tstaustau_samples_mc.json
 INDIR=/lustre/ncg.ingrid.pt/cmst3/store/user/cbeiraod/14_08_06_2l2nu_EDMtuples_merged
-#runLocalAnalysisOverSamples.py -e runTStauStauDDBkg -j $JSONFILE -d $INDIR/ -o $OUTDIR/ -c runAnalysis_cfg.py.templ -p "@doPrompt=False @saveSummaryTree=False @runSystematics=False @exclusiveRun=True @applyScaleFactors=False @doSVfit=False" -s 8nh
+runLocalAnalysisOverSamples.py -e runTStauStauDDBkg -j $JSONFILE -d $INDIR/ -o $OUTDIR/ -c runAnalysis_cfg.py.templ -p "@saveSummaryTree=False @runSystematics=False @exclusiveRun=True @applyScaleFactors=False @doSVfit=False" -s 8nh
 
 
 JSONFILE=tstaustau_samples_data.json
 INDIR=/lustre/ncg.ingrid.pt/cmslocal/samples/tempSingleLepton
-#runLocalAnalysisOverSamples.py -e runTStauStauDDBkg -j $JSONFILE -d $INDIR/ -o $OUTDIR/ -c runAnalysis_cfg.py.templ -p "@doPrompt=False @saveSummaryTree=False @runSystematics=False @exclusiveRun=True @applyScaleFactors=False @doSVfit=False" -s 8nh
-
-#Prompt Rate
-OUTDIR=/lustre/ncg.ingrid.pt/cmslocal/cbeiraod/DDBkgStudyPR
-
-JSONFILE=tstaustau_samples_mc.json
 INDIR=/lustre/ncg.ingrid.pt/cmst3/store/user/cbeiraod/14_08_06_2l2nu_EDMtuples_merged
-#runLocalAnalysisOverSamples.py -e runTStauStauDDBkg -j $JSONFILE -d $INDIR/ -o $OUTDIR/ -c runAnalysis_cfg.py.templ -p "@doPrompt=True @saveSummaryTree=False @runSystematics=False @exclusiveRun=True @applyScaleFactors=False @doSVfit=False" -s 8nh
-
-
-JSONFILE=tstaustau_samples_data.json
-INDIR=/lustre/ncg.ingrid.pt/cmslocal/samples/tempSingleLepton
-runLocalAnalysisOverSamples.py -e runTStauStauDDBkg -j $JSONFILE -d $INDIR/ -o $OUTDIR/ -c runAnalysis_cfg.py.templ -p "@doPrompt=True @saveSummaryTree=False @runSystematics=False @exclusiveRun=True @applyScaleFactors=False @doSVfit=False" -s 8nh
+#runLocalAnalysisOverSamples.py -e runTStauStauDDBkg -j $JSONFILE -d $INDIR/ -o $OUTDIR/ -c runAnalysis_cfg.py.templ -p "@saveSummaryTree=False @runSystematics=False @exclusiveRun=True @applyScaleFactors=False @doSVfit=False" -s 8nh
