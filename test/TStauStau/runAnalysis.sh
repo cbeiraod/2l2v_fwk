3f9b26485f761310e52685365ce0a482571fc340
Cristovao da Cruz e Silva <cbeiraod@cern.ch>
2015-06-26 15:51:57 +0100

    Added option to run MC closure only

--- test/TStauStau/runAnalysis.sh
+++ test/TStauStau/runAnalysis.sh
@@ -2,3 +2,6 @@
 
 #runLocalAnalysisOverSamples.py -e runTStauStauAnalysisFWLite -j tstaustau_samples_full.json -d  /lustre/ncg.ingrid.pt/cmst3/store/user/cbeiraod/14_08_06_2l2nu_EDMtuples_merged/ -o /lustre/ncg.ingrid.pt/cmslocal/cbeiraod/NewObject/ -c runAnalysis_cfg.py.templ -p "@saveSummaryTree=True @runSystematics=False @exclusiveRun=True @applyScaleFactors=True @doSVfit=True" -s 8nh
 runLocalAnalysisOverSamples.py -e runTStauStauAnalysisFWLite -j tstaustau_samples_full.json -d  /lustre/ncg.ingrid.pt/cmst3/store/user/cbeiraod/14_08_06_2l2nu_EDMtuples_merged/ -o /lustre/ncg.ingrid.pt/cmslocal/cbeiraod/NewObject/ -c runAnalysis_cfg.py.templ -p "@saveSummaryTree=True @runSystematics=False @exclusiveRun=True @applyScaleFactors=True @doSVfit=False" -s 8nh
+
+## MCClosure
+#runLocalAnalysisOverSamples.py -e runTStauStauAnalysisFWLite -j tstaustau_samples_MCClosure.json -d  /lustre/ncg.ingrid.pt/cmst3/store/user/cbeiraod/14_08_06_2l2nu_EDMtuples_merged/ -o /lustre/ncg.ingrid.pt/cmslocal/cbeiraod/NewObject/ -c runAnalysis_cfg.py.templ -p "@saveSummaryTree=True @runSystematics=False @exclusiveRun=True @applyScaleFactors=True @doSVfit=False" -s 8nh
