fc85c48690b811824b259e5a4a6c4b52c16f5477
Cristovao da Cruz e Silva <cbeiraod@cern.ch>
2015-08-04 12:46:09 +0100

    Fixed runCombine to run on the lowest neutralino mass points

--- test/TStauStau/Limits/runCombine.sh
+++ test/TStauStau/Limits/runCombine.sh
@@ -1,15 +1,30 @@
 #!/bin/bash
 
+if [[ -z "$1" ]]; then
+  echo "You must specify an input directory"
+  exit
+fi
+
+DIRECTORY="$(readlink -m $1)"
+CWD="$(pwd)"
+
+cd $DIRECTORY
+
 for stauM in {50..500..10}
 do
   for neutM in {0..480..10}
   do
+    if [[ neutM -eq 0 ]]; then
+      neutM=1
+    fi
     echo "Stau: " $stauM "; Neutralino: " $neutM
 
     file=""
-    printf -v file './Results/SignalPoint_%d%03d.txt' "$stauM" "$neutM"
+    printf -v file 'SignalPoint_%d%03d.txt' "$stauM" "$neutM"
     if [[ -f $file ]]; then
       combine -M Asymptotic --run=blind $file -n S$stauM-N$neutM
     fi
   done
 done
+
+cd $CWD
