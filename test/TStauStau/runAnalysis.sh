7415660aec90c551df893a02d3b75fbe39313d81
Cristovao da Cruz e Silva <cbeiraod@cern.ch>
2015-09-30 16:35:20 +0100

    Enabled systematics and changed output directory

--- test/TStauStau/runAnalysis.sh
+++ test/TStauStau/runAnalysis.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #runLocalAnalysisOverSamples.py -e runTStauStauAnalysisFWLite -j tstaustau_samples_full.json -d  /lustre/ncg.ingrid.pt/cmst3/store/user/cbeiraod/14_08_06_2l2nu_EDMtuples_merged/ -o /lustre/ncg.ingrid.pt/cmslocal/cbeiraod/NewObject/ -c runAnalysis_cfg.py.templ -p "@saveSummaryTree=True @runSystematics=False @exclusiveRun=True @applyScaleFactors=True @doSVfit=True" -s 8nh
-runLocalAnalysisOverSamples.py -e runTStauStauAnalysisFWLite -j tstaustau_samples_full.json -d  /lustre/ncg.ingrid.pt/cmst3/store/user/cbeiraod/14_08_06_2l2nu_EDMtuples_merged/ -o /lustre/ncg.ingrid.pt/cmslocal/cbeiraod/NewObject/ -c runAnalysis_cfg.py.templ -p "@saveSummaryTree=True @runSystematics=False @exclusiveRun=True @applyScaleFactors=True @doSVfit=False" -s 8nh
+runLocalAnalysisOverSamples.py -e runTStauStauAnalysisFWLite -j tstaustau_samples_full.json -d  /lustre/ncg.ingrid.pt/cmst3/store/user/cbeiraod/14_08_06_2l2nu_EDMtuples_merged/ -o /lustre/ncg.ingrid.pt/cmslocal/cbeiraod/WithSyst_Prompt/ -c runAnalysis_cfg.py.templ -p "@saveSummaryTree=True @runSystematics=True @exclusiveRun=True @applyScaleFactors=True @doSVfit=False" -s 8nh
 
 ## MCClosure
 #runLocalAnalysisOverSamples.py -e runTStauStauAnalysisFWLite -j tstaustau_samples_MCClosure.json -d  /lustre/ncg.ingrid.pt/cmst3/store/user/cbeiraod/14_08_06_2l2nu_EDMtuples_merged/ -o /lustre/ncg.ingrid.pt/cmslocal/cbeiraod/NewObject/ -c runAnalysis_cfg.py.templ -p "@saveSummaryTree=True @runSystematics=False @exclusiveRun=True @applyScaleFactors=True @doSVfit=False" -s 8nh
