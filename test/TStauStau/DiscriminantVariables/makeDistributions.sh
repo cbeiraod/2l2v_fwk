8d1d8e52b09b224a348d256df3a08f6776cdd312
Cristovao da Cruz e Silva <cbeiraod@cern.ch>
2015-11-09 08:51:04 +0000

    Updated distribution plotter

--- test/TStauStau/DiscriminantVariables/makeDistributions.sh
+++ test/TStauStau/DiscriminantVariables/makeDistributions.sh
@@ -2,5 +2,8 @@
 
 OUTDIR=/lstore/cms/cbeiraod/Distributions
 INDIR=/lstore/cms/cbeiraod/NewObject
+OUTDIR=./
+INDIR=/lstore/cms/cbeiraod/FinalUpdated_Full
+INDIR=/lstore/cms/cbeiraod/FinalUpdated
 
-makeDistributions --json tstaustau_samples_full.json --variables variables.json --inDir $INDIR/ --outDir $OUTDIR/ --signalSelection "stauMass-neutralinoMass==50" --extraSignal "stauMass-neutralinoMass==200" --plotExt .png --plotExt .root --baseSelection "selected" --iLumi 19672 --pointVar "stauMass*1000+neutralinoMass" --sigXSec 0.1 --sigNInitEvents 10000 --unblind
+makeDistributions --json tstaustau_samples_full.json --variables variables.json --inDir $INDIR/ --outDir $OUTDIR/ --signalSelection "d_stauMass-d_neutralinoMass==50" --extraSignal "d_stauMass-d_neutralinoMass==200" --plotExt .png --plotExt .C --baseSelection "b_selected" --iLumi 19672 --pointVar "d_stauMass*1000+d_neutralinoMass" --sigXSec 0.1 --sigNInitEvents 10000 --unblind --weightVar "d_weight" --puweightVar "d_PUweight"
